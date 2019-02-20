807d8a9238df1f5cc0b9167bc3e1cfcac3a0962c
Pankushg <39335738+Pankushg@users.noreply.github.com>
2019-02-20 17:32:26 +0530

    Add files via upload

--- login.component.ts
+++ login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl,FormGroup, Validators} from '@angular/forms';
+import { LoginService } from '../shared/login.service'
 
 @Component({
   selector: 'app-login',
@@ -11,13 +12,19 @@ export class LoginComponent implements OnInit {
   loginForm = new FormGroup({
     phone : new FormControl('',[Validators.required,Validators.pattern('[1-9][0-9]*'),Validators.minLength(10), Validators.maxLength(10)]),
   });
-  constructor() { }
+  constructor(private loginService : LoginService) { }
 
   ngOnInit() {
   }
 
   login(){
-    console.log(this.loginForm)
+    console.log('login component')
+    this.loginService.validate().subscribe(
+        data=>{
+          console.log('after otp login component')
+          console.log(data)
+        }
+    )
   }
 
 }
